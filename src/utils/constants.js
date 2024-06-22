c4f9ab0a2d6ff8687d4de36ebac5fef0b63f65d4
realsumitkumar <realsumitkumar@gmail.com>
2024-06-22 13:55:30 +0530

    updated

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,4 +5,4 @@ export const LOGO_URL =
   "https://img.freepik.com/free-vector/food-shopping-logo-template-design_460848-10299.jpg?size=626&ext=jpg&ga=GA1.1.1224184972.1714953600&semt=ais";
 
 export const MENU_URL =
-  "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.6713752&lng=77.2692261&restaurantId=";
+  "https://cors-anywhere.herokuapp.com/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.6713752&lng=77.2692261&restaurantId=";
